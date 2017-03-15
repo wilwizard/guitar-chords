c635b76338dd83c2f92fa369f2efe6ff7aa49370
Will Bendix <wilwizard4@gmail.com>
2017-03-14 22:26:20 -0700

    Guitar can identify major and minor chords

--- app/models/guitar.js
+++ app/models/guitar.js
@@ -34,25 +34,82 @@ numberToNote = {
 };
 
 
-var Guitar = function(options){
+Number.prototype.mod = function(n) {
+    return ((this%n)+n)%n;
+};
 
+var makeRoot = function(notes, root){
+  var temp = [];
+  for(var y = 0; y < notes.length; y++){
+    var note = notes[y];
+    temp.push((note - root).mod(12));
+  }
+  return temp;
+};
+
+var isMajor = function(notes) {
+  var hasFifth = _.contains(notes, 7);
+  var hasThird = _.contains(notes, 4);
+  return hasFifth & hasThird;
+};
+
+var isMinor = function(notes) {
+  var hasFifth = _.contains(notes, 7);
+  var hasMinorThird = _.contains(notes, 3);
+  return hasFifth & hasMinorThird;
+};
+
+var String = function(options){
   function addTabs(string, tab){
-    return (string + tab)%12;
+    return (string + parseInt(tab)) % 12;
   }
 
-  this.strings = ko.observableArray(_.map(options.strings, function(str){
-    var stringNum = noteToNumber[str.string]
-    var noteNum = addTabs(stringNum, str.tab);
-    return _.extend(str, {stringNum: stringNum, noteNum: noteNum, note: numberToNote[noteNum]});
+  var self = this;
+
+  this.string = ko.observable(options.string);
+  this.tab = ko.observable(options.tab);
+
+  this.stringNum = ko.computed(function(){
+    return noteToNumber[self.string()];
+  }, this);
+
+  this.noteNum = ko.computed(function(){
+    return addTabs(this.stringNum(), this.tab());
+  }, this);
+
+  this.note = ko.computed(function(){
+    return numberToNote[this.noteNum()];
+  }, this);
+};
+
+var Guitar = function(options){
+  var self = this;
+  self.strings = ko.observableArray(_.map(options.strings, function(str){
+    return new String(str);
   }));
 
+  self.stringNumbers = ko.computed(function(){
+    return _.map(self.strings(), function(str){
+      return str.noteNum();  
+    });
+  }); 
+
+  self.currentChords = ko.computed(function() {
+    var numbers = _.uniq(self.stringNumbers());
+    var chords = [];
+
+    for (var x = 0; x < numbers.length; x++) {
+      var ajusted = makeRoot(numbers, numbers[x]);
+      console.log(ajusted);
+      var root = numbers[x];
+      if (isMajor(ajusted)) {
+        chords.push(numberToNote[root] + 'maj');
+      } else if (isMinor(ajusted)) {
+        chords.push(numberToNote[root] + 'min');
+      }
+    }
+
+    return chords;
+  });
 };
 
-Guitar.prototype.changeNote = function(string, fret){
-  this.fretInts[string] = (fret + this.tuningInts[string])%12;
-  this.fretNotes[string] = numberToNote[this.fretInts[string]];
-};
-
-Guitar.prototype.currentChords = function(string, fret){
-
-}
